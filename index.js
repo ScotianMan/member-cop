4c5552124e47aa795ac08e7bbe932d91f5aa3bbb
James Walsh <jameswalsh@Jamess-MacBook-Pro-6.local>
2018-10-03 17:56:26 -0300

    Grab org and add axios

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * This is the entry point for your Probot App.
  * @param {import('probot').Application} app - Probot's Application class.
  */
+const axios = require('axios');
+
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
@@ -9,7 +11,24 @@ module.exports = app => {
   app.on('issue_comment', async context => {
     app.log(context.payload)
 
-    if (context['payload']['sender']['login'] != 'member-cop[bot]'){
+    sender = context['payload']['sender']['login']
+    if (sender != 'member-cop[bot]'){
+      org = context['payload']['issue']['html_url']
+      var frontRegex = /https:\/\/github.com\//gi;
+      org.replace(frontRegex, '')
+      var backRegex = /\/.*/gi;
+      org.replace(backRegex, '')
+      app.log(org)
+      // axios.get('https://api.github.com/orgs/' + org + '/members/' + sender)
+      // .then(response => {
+      //   console.log(response.data.url);
+      //   console.log(response.data.explanation);
+      // })
+      // .catch(error => {
+      //   console.log(error);
+      // });
+
+
       const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
       return context.github.issues.createComment(issueComment)
     }
