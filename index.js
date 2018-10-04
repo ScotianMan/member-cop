c3e04077bb867e284e1f6c7049be114aa93a272c
James Walsh <jameswalsh@Jamess-MacBook-Pro-6.local>
2018-10-03 21:38:49 -0300

    Finalize initial features

--- index.js
+++ index.js
@@ -2,60 +2,62 @@
  * This is the entry point for your Probot App.
  * @param {import('probot').Application} app - Probot's Application class.
  */
-const axios = require('axios');
+const axios = require('axios')
+const HTMLParser = require('node-html-parser')
 
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
 
   app.on('issue_comment', async context => {
-    app.log(context.payload)
 
     sender = context['payload']['sender']['login']
     bot = context['payload']['sender']['type']
-    if (bot != 'bot'){
+    if (bot != 'Bot'){
       org = context['payload']['issue']['html_url']
       var frontRegex = /https:\/\/github.com\//g
       org = org.replace(frontRegex, '')
       var backRegex = /\/.*/g
       org = org.replace(backRegex, '')
       url = 'https://api.github.com/orgs/' + org + '/members/' + sender
-      app.log('====')
-      app.log(url)
-      app.log('====')
       axios.get(url)
-        .then(response => {
-          // We dont really want to do anything when members comment 
-        })
-        .catch(error => {
-          private_or_not_member_message = 'User does not exist or is not a public member of the organization'
-          err_msg = error.response.data.message
-          if (private_or_not_member_message = err_msg){
-            // Here we are either sure this person is not a member or a private member. 
-            // Lets make sure they are not a private member
-            comments_url = context['payload']['issue']['comments_url']
-            axios.get(comments_url)
-            .then(response => {
-              // Does the page show the commenter to be a member of the repo?
-              app.log(response)
-            })
-            .catch(error => {
-              // booo
-              app.log('Somthing went wrong getting the issue page')
-            })
-
-          }
-        })
-
-
-      const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-      return context.github.issues.createComment(issueComment)
+      .then(response => {
+        // We dont really want to do anything when members comment 
+      })
+      .catch(error => {
+        private_or_not_member_message = 'User does not exist or is not a public member of the organization'
+        err_msg = error.response.data.message
+        if (private_or_not_member_message = err_msg){
+          // Here we are either sure this person is not a member or a private member. 
+          // Lets make sure they are not a private member
+          comments_url = context['payload']['comment']['html_url']
+          axios.get(comments_url)
+          .then(response => {
+            // Lets get clever and scrape the HTML and see if this is a private member or not
+            // get the id of the comment parent div
+            var comment_regex = /.+?(?=#)/g
+            comment_id_str = comments_url.replace(comment_regex, '')
+            // Does the page show the commenter to be a member of the repo?
+            const root = HTMLParser.parse(response.data)
+            comment_html = root.querySelector(comment_id_str).toString()
+            owner_str = 'This user is the owner of the'
+            member_str = 'You are a member of the'
+            // TODO: Has this person posted in this comments before? if so we should only have the bot reply once
+            if (!comment_html.includes(owner_str) && !comment_html.includes(member_str)){
+              // Here we finally know if the person posting the comment is a member.... even private ones muahahahaha ;-)
+              const issueComment = context.issue({
+                body: 'Hey @' + sender + 
+                ', thanks for expressing your interest. We would love your help with this issue.' + 
+                ' Please follow these <a href="https://github.com/ifmeorg/ifme/wiki/Join-Our-Slack">next steps</a>' +
+                ' to join our contributor community.'
+              })
+              return context.github.issues.createComment(issueComment)
+            }
+          })
+          .catch(error => {
+            // booo
+            app.log('Somthing went wrong getting the issue page', error)
+          })
+        }
+      })
     }
   })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
