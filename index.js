ef3b174ab2d8c9a03536909018a357e529b9b478
James Walsh <jameswalsh@Jamess-MacBook-Pro-6.local>
2018-10-13 22:58:00 -0300

    Change GH API call

--- index.js
+++ index.js
@@ -86,9 +86,9 @@ module.exports = app => {
               app.log('Private member comment or possible someone who doesnt exist lol')
               context.github.orgs.getMembers({org: 'ifmeorg', per_page: 100}).then(
                 result => {
+                  app.log(result.data)
                   if (result.status == 204){
                     app.log("Winner Winner, member of repo")
-                    app.log(result.data)
 
                   } else if (result.status == 404){
                     app.log("Not a member of the repo")
